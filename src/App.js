908e27ed36f082b39fb94f5d6c2fb5f3aa00d623
Akankshaajadhav <161219374+Akankshaajadhav@users.noreply.github.com>
2025-04-10 14:36:21 +0530

    Added frontend for assessment page for employee and team lead

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddAppraisalCycle from "./components/AddAppraisalCycle";
 import Questionnaire from "./components/Questionnaire";
 import HRLandingPage from "./components/HRLandingPage";
 import Login from "./components/LoginCompo"; 
-import DropdownPage from "./components/LeadLandingPage";                      //Change it as per your file
+import DropdownPage from "./components/employee_assessment";                      //Change it as per your file
 
 export default function App() {
   return (
